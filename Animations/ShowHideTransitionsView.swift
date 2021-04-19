1fc2c20e29e3c8f01710c5c95811961eccb6b265
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-19 13:44:25 +0100

    Add ShowHideTransitionsView.

---
+++ Animations/ShowHideTransitionsView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowHideTransitionsView.swift
+//  Animations
+//
+//  Created by Pranav Kasetti on 19/04/2021.
+//
+
+import SwiftUI
+
+struct ShowHideTransitionsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ShowHideTransitionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowHideTransitionsView()
+    }
+}
